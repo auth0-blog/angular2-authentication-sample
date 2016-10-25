c95ca40eb340499457242c61551e5c113fa1e22a
kukicadnan <kukicadnan@gmail.com>
2016-10-25 16:05:03 -0700

    Add Angular 2 authentication with Lock sample
    Alternative way to authenticate with Auth0 lock.

---
+++ src/common/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { tokenNotExpired } from 'angular2-jwt';
+import { Router } from '@angular/router';
+
+declare var Auth0Lock: any;
+
+@Injectable()
+export class AuthService {
+
+  lock = new Auth0Lock('YOUR-AUTH0-CLIENT-ID', 'YOUR-AUTH0-DOMAIN.auth0.com');
+
+  constructor(private router: Router) {
+    this.lock.on('authenticated', (authResult: any) => {
+      localStorage.setItem('id_token', authResult.idToken);
+
+      this.lock.getProfile(authResult.idToken, (error: any, profile: any) => {
+        if (error) {
+          console.log(error);
+        }
+
+        localStorage.setItem('profile', JSON.stringify(profile));
+        this.router.navigateByUrl('/home');
+      });
+
+      this.lock.hide();
+    });
+  }
+
+  login() {
+    this.lock.show();
+  }
+
+  logout() {
+    // To log out, just remove the token and profile
+    // from local storage
+    localStorage.removeItem('profile');
+    localStorage.removeItem('id_token');
+
+    // Send the user back to the dashboard after logout
+    this.router.navigateByUrl('/login');
+  }
+
+  loggedIn() {
+    return tokenNotExpired();
+  }
+}
